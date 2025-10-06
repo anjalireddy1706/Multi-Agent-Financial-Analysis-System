ba1f62e79c0ee5e6c0d00ce7c156331335c125cf
anjalireddy1706 <57055215+anjalireddy1706@users.noreply.github.com>
2025-10-06 19:14:32 +0100

    Add files via upload

---
+++ financial_agent.py
@@ -0,0 +1,46 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get Groq API key
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+# Web search agent
+web_search_agent = Agent(
+    name="Web Search Agent",
+    role="Search the web for the information",
+    model=Groq(id="llama-3.3-70b-versatile", api_key=groq_api_key),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+# Financial Agent
+finance_agent = Agent(
+    name="Finance Agent",
+    model=Groq(id="llama-3.3-70b-versatile", api_key=groq_api_key),
+    tools=[
+        YFinanceTools(stock_price=True, analyst_recommendations=True, 
+                     stock_fundamentals=True, company_news=True),
+    ],
+    instructions=["Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+# Multi-agent coordinator
+multi_ai_agent = Agent(
+    model=Groq(id="llama-3.3-70b-versatile", api_key=groq_api_key),
+    team=[web_search_agent, finance_agent],
+    instructions=["Always include sources", "Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent.print_response("Summarize analyst recommendation and share the latest news for NVDA", stream=True)
